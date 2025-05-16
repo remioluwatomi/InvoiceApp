f1b8cfae230dd3b8fc1873a9c3adc8221276831b
tomi <remioluwatomicephas@gmail.com>
2025-05-16 10:17:16 +0100

    feat: implemented Client model

---
+++ Models/Client.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace InvoiceApp.Models;
+
+public class Client
+{
+
+    public int Id { get; set; }
+    public required string ClientName { get; set; }
+    [EmailAddress]
+    public required string ClientEmail { get; set; }
+    public required string StreetAddress { get; set; }
+    public required string City { get; set; }
+    public required string PostCode { get; set; }
+    public required string Country { get; set; }
+
+    // public virtual Invoice Invoice {get; set;}
+
+}
