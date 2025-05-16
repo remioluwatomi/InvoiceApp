a470742a9e27cbe4283e8cd65d2e761fd3fc1b5a
tomi <remioluwatomicephas@gmail.com>
2025-05-16 10:01:47 +0100

    feat: implemented Biller model

---
+++ Models/Biller.cs
@@ -0,0 +1,14 @@
+﻿namespace InvoiceApp.Models;
+
+public class Biller
+{
+
+    public int Id { get; set; }
+    public required string StreetAddress { get; set; }
+    public required string City { get; set; }
+    public required string PostCode { get; set; }
+    public required string Country { get; set; }
+
+    // public virtual Invoice Invoice {get; set;}
+
+}
