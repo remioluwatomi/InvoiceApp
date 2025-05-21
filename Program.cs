57c9b9fbe8d972ebb8028608a4eb59f45c100426
tomi <remioluwatomicephas@gmail.com>
2025-05-21 09:58:41 +0100

    feat: implemented initial migration and data mapping

--- Program.cs
+++ Program.cs
@@ -1,14 +1,26 @@
+using InvoiceApp.Data;
+using Microsoft.EntityFrameworkCore;
+using InvoiceApp.Mapping;
+using AutoMapper;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 
+builder.Services.AddDbContext<InvoiceContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddAutoMapper(typeof(MappingConfigProfile));
+
 var app = builder.Build();
+
+app.UseStatusCodePagesWithReExecute("/Error/{0}");
+app.UseRouting();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Error");
+    app.UseExceptionHandler("/Error/500");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
